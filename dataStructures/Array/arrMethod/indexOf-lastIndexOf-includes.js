c07097a5820def93556be24f15a8c40bcd8d9f0e
Maksim hmelnickiy <maksimhmelnickiy@ip-192-168-31-122.eu-central-1.compute.internal>
2022-09-29 15:06:17 +0300

    arr method

--- dataStructures/Array/arrMethod/indexOf-lastIndexOf-includes.js
+++ dataStructures/Array/arrMethod/indexOf-lastIndexOf-includes.js
@@ -1,10 +1,13 @@
-//arr.indexOf(item, from) ищет item, начиная с индекса from, и возвращает индекс, на котором был найден искомый элемент, в противном случае -1.
-//arr.lastIndexOf(item, from) – то же самое, но ищет справа налево.
-//arr.includes(item, from) – ищет item, начиная с индекса from, и возвращает true, если поиск успешен.
+//⁡⁢⁣⁣arr.indexOf(item, from)⁡ ищет ⁡⁣⁣⁢item⁡, начиная с индекса ⁡⁣⁣⁢from⁡, и возвращает индекс, на котором был найден искомый элемент, в противном случае ⁡⁣⁢⁣-1⁡.
+//⁡⁢⁣⁣arr.lastIndexOf(item, from)⁡ – то же самое, но ищет ⁡⁣⁣⁢справа налево⁡.
+//⁡⁢⁣⁣arr.includes(item, from)⁡ – ищет ⁡⁣⁣⁢item⁡, начиная с индекса ⁡⁣⁣⁢from⁡, и возвращает ⁡⁣⁣⁢true⁡, если поиск успешен.
 
 let arr = ['first', 'second', 'third', 'fourth']
-document.write(`<br/> <b>arr.indexOf(item, from)</b> </br>`)
-document.write(`before: ${arr.indexOf('third', 0)}`)
+console.log('indexOf', arr.indexOf('fourth', 1)) // Возвращает ⁡⁣⁣⁢индекс⁡ элемента в данном случае будет ⁡⁣⁢⁣3⁡,
 
-document.write(`<br/> <b>arr.includes(item, from)</b> </br>`)
-document.write(`before: ${arr.includes('third', 0)}`)
+console.log('includes', arr.includes('third')); // Вернет ⁡⁣⁣⁢true⁡,
+
+// также методы ⁡⁢⁣⁣include, indexOf,⁡ ⁡⁢⁣⁣lastIndexOf⁡  можно использовать для ⁡⁣⁣⁢строк⁡.
+let str = 'lorem ipsum for my cat';
+console.log('includes string', str.includes('for')); // вернет ⁡⁣⁣⁢true⁡ .
+console.log('indexOf string', str.indexOf('cat')) // Вернет индекс ⁡⁣⁢⁣19⁡ /
--- dataStructures/Array/arrMethod/splice.js
+++ dataStructures/Array/arrMethod/splice.js
@@ -1,28 +1,29 @@
-// delete - удаляет значение с заданным ключем , но длина массива остается прежней
-document.write('</br><b>DELETE</b></br>')
+// ⁡⁢⁣⁣delete⁡ - удаляет значение с заданным ⁡⁣⁣⁢ключем⁡ , но длина массива остается ⁡⁣⁣⁢прежней⁡
+
 let arr = ["I", "go", "home"];
 
-document.write(`<b>delete</b>: before - ${arr} </br>`)
+console.log(arr)
 
-delete arr[1]; // удалить "go"
+delete arr[1]; // удалить "⁡⁢⁣⁣go⁡"
 
-document.write(`<b>delete</b>: after - ${arr}`)
+console.log('Delete: ', arr)
 
 
-//Метод arr.splice(str) – это универсальный «швейцарский нож» для работы с массивами. Умеет всё: добавлять, удалять и заменять элементы.
+// Метод ⁡⁢⁣⁣arr.splice(str)⁡ – это универсальный «швейцарский нож» для работы с массивами. 
+// Умеет всё: добавлять, удалять и заменять элементы.
 
-document.write('</br></br></br><b>SPLICE</b></br>')
 
 let arrSplice = ["Я", "изучаю", "JavaScript"];
 
-document.write(`<b>delete</b>: before - ${arrSplice} </br>`)
+console.log(arrSplice)
 
-arrSplice.splice(1, 1); // начиная с позиции 1, удалить 1 элемент
+arrSplice.splice(2, 1); // начиная с позиции 2⁡, удалить⁡⁣⁢⁣ 1⁡ элемент
 
-document.write(`<b>delete</b>: after - ${arrSplice}`)
+console.log(arrSplice)
 
-//Метод splice также может вставлять элементы без удаления, для этого достаточно установить deleteCount в 0:
+//Метод ⁡⁢⁣⁣splice⁡ также может вставлять элементы без у⁡⁢⁢⁢дален⁡ия, для этого достаточно установить ⁡⁣⁣⁢deleteCount⁡ в ⁡⁣⁢⁣0⁡:
 
 let arrSplice2 = ["Я", "изучаю", "JavaScript"];
-arrSplice2.splice(-1,0, 'сложный', 'язык') //В этом и в других методах массива допускается использование отрицательного индекса. Он позволяет начать отсчёт элементов с конца
-document.write(`</br>arrSplice2.splice(2,0, 'сложный', 'язык')  <b>Result:</b>  ${arrSplice2}`)
+arrSplice2.splice(-1,0, 'сложный', 'язык') //В этом и в других методах массива допускается использование отрицательного индекса. 
+// Он позволяет начать отсчёт элементов с конца
+console.log('Добавить: ', arrSplice2)
